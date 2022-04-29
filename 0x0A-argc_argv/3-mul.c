25e2a44f822ae99d2ddb8f3f1e45626fe46064cb
sintayehu-244 <sintayehutilahun89@yahoo.com>
2022-04-29 14:01:18 -0700

    commit iron 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * main - multiply two numbers
- * @argc: arguement count
- * @argv: array of pointers to arguement strings
- * Return: result of multiplication or 1
- **/
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
+ */
+
 int main(int argc, char *argv[])
 {
-int a, b, result;
+int i, j;
 
-if (argc <= 2)
+if (argc == 1 || argc == 2)
 {
 printf("Error\n");
 return (1);
 }
-/* atoi convert a string to an int */
-a = atoi(argv[1]);
-b = atoi(argv[2]);
-result = a * b;
-printf("%d\n", result);
+else
+{
+j = 1;
+
+for (i = 1; i < 3; i++)
+j *= atoi(argv[i]);
+
+printf("%d\n", j);
+}
+
 return (0);
 }
