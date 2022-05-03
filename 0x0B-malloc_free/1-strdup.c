a00973ff71627631f4eec3bbc46e99cb9e9df2a4
sintayehu-244 <sintayehutilahun89@yahoo.com>
2022-05-03 11:47:53 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,33 +1,58 @@
-#include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
+
 /**
- * _strdup - malloc new str and copy *str into it
- * @str: the string we copy
- * Return: pointer to new string
+ * _strlen - returns the length of a string.
+ * @s: string.
+ *
+ * Return: Length of @s.
+ */
+int _strlen(char *s)
+{
+int i = 0;
+int cont = 0;
+
+while (s[i] != '\0')
+{
+cont++;
+i++;
+}
+return (cont);
+}
+
+/**
+ * _strcpy - copy the string pointed to by src inluding the null byte
+ * to the buffer pointed to by dest.
+ * @dest: destination.
+ * @src: source.
+ *
+ * Return: @direction = pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *direction = dest;
+
+while (*src)
+*dest++ = *src++;
+*dest = 0;
+return (direction);
+}
+
+/**
+ * _strdup - returns a pointer to a new string which is a duplicate of str.
+ * @str: source string to duplicate.
+ *
+ * Return: pointer to the duplicated string.
+ * or NULL if str = NULL or if insufficient memory was available.
  */
 char *_strdup(char *str)
 {
-unsigned int i;
-int count = 0;
-char *dest;
+char *aux;
 
 if (str == NULL)
 return (NULL);
-for (i = 0; str[i]; i++)
-{
-count++;
-}
-count += 1;
-dest = malloc(count * sizeof(char));
-if (dest == NULL)
-{
+aux = malloc(sizeof(char) * (_strlen(str) + 1));
+if (aux == NULL)
 return (NULL);
-}
-for (i = 0; str[i] != '\0'; i++)
-{
-dest[i] = str[i];
-}
-dest[i] = str[i];
-return (dest);
+return (_strcpy(aux, str));
 }
