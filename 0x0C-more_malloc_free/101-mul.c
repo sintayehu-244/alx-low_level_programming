458555af944c15ff021ea634b5482135df111ca4
sintayehu-244 <sintayehutilahun89@yahoo.com>
2022-05-06 11:35:08 -0700

    Write a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,7 +7,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
