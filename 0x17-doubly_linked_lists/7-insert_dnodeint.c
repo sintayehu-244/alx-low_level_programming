dd541b37253ffd63ebe4976b510047027becee76
sintayehu-244 <101401891+sintayehu-244@users.noreply.github.com>
2022-07-07 18:07:29 +0300

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,43 @@
 #include "lists.h"
 /**
- * *insert_dnodeint_at_index - function inserts new node at given position
- *
- *@h: pointer to pointer of head of linked list
- *@idx: unsigned int index position
- *@n: int data
- *
- * Return: address of new element or NULL if failed
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: a double pointer to the head of the DLL
+ * @idx: index of the list where the new node should be added
+ * @n: the content of the new node to be added
+ * Return: the address of the new node or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *currentnode;
-	dlistint_t *newnode;
+	dlistint_t *new_node;
+	dlistint_t *aux_h;
 
-	while (*h)
+	/* case asked idx is 0 or h is null at the beggining, recursion */
+	if (idx == 0)
 	{
-		newnode = malloc(sizeof(dlistint_t));
-		currentnode = *h;
-		newnode->n = n;
-
-		if (idx == 0)
-		{
-			newnode->next = NULL;
-			return (newnode);
-		}
-		while (currentnode->next != NULL && idx - 1 > 0)
-		{
-			currentnode = currentnode->next;
-			idx--;
-		}
-		if (currentnode->next == NULL)
-		{
-			free(newnode);
-			return (NULL);
-		}
-		newnode->next = currentnode->next;
-		currentnode->next = newnode;
-		return (newnode);
+		return (add_dnodeint(h, n));
 	}
-	return (NULL);
+	/* traverse the list */
+	aux_h = *h;
+	while (idx > 1)
+	{
+		aux_h = aux_h->next;
+		if (aux_h == NULL)
+			return (NULL);
+		idx--;
+	}
+	/* case at the end, recursion */
+	if (aux_h->next == NULL)
+	{
+		return (add_dnodeint_end(h, n));
+	}
+	/* idx found then add new_node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = aux_h;
+	new_node->next = aux_h->next;
+	aux_h->next->prev = new_node;
+	aux_h->next = new_node;
+	return (new_node);
 }
