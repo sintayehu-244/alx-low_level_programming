92c60458cb7ea1bd85705f49f8376ae7b5a38e4c
sintayehu-244 <101401891+sintayehu-244@users.noreply.github.com>
2022-07-07 18:09:40 +0300

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,38 +1,37 @@
 #include "lists.h"
 /**
- * *add_dnodeint_end - function adds new node to end of list
- *
- *@head: pointer to pointer of head of linked list
- *@n: const int pointer
- *
- * Return: address of new element, or NULL if failed
+ * add_dnodeint_end - adds a new node at the end of a DLL
+ * @head: double pointer to the beggining of the list
+ * @n: content of the new node to be add
+ * Return: the address of the new element or NULL if it failed
  */
-
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *last = *head;
-	dlistint_t *newnode = NULL;
+	dlistint_t *last_node;
+	dlistint_t *addrs_new_node;
 
-	newnode = malloc(sizeof(dlistint_t));
-	if (newnode == NULL)
+	addrs_new_node = malloc(sizeof(dlistint_t));
+	if (addrs_new_node == NULL)
 		return (NULL);
-
-	newnode->n = n;
-	newnode->next = NULL;
-
-	if ((*head) == NULL)
+	/* add data to addrs_new_node */
+	addrs_new_node->next = NULL;
+	addrs_new_node->n = n;
+	/* make last_node the head to traverse the list till the end */
+	last_node = *head;
+	if (last_node == NULL)
 	{
-		newnode->prev = NULL;
-		(*head) = newnode;
-		return (newnode);
+		/* if the list is null */
+		/* addrs_new_node becomes the head of the list*/
+		addrs_new_node->prev = NULL;
+		*head = addrs_new_node;
+		return (addrs_new_node);
 	}
-
-	while (last->next != NULL)
-		last = last->next;
-	last->next = newnode;
-
-	if (newnode->next != NULL)
-		newnode->next->prev = newnode;
-
-	return (newnode);
+	/* else, traverse till last node */
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+	/* when we get the end of the list change next of last_node*/
+	/* with the new_node */
+	last_node->next = addrs_new_node;
+	addrs_new_node->prev = last_node;
+	return (addrs_new_node);
 }
